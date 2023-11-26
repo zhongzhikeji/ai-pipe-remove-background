2f6d7b6e94782599027554f6c6f232670738b779
crapthings <crapthings@gmail.com>
2023-11-26 12:18:46 +0800

    we don't have to call webhook ourself

--- runpod_app.py
+++ runpod_app.py
@@ -19,11 +19,6 @@ def upload_image (url, image):
     response = requests.put(url, data = buff_png(image), headers = { 'Content-Type': 'image/png' })
     response.raise_for_status()
 
-def webhook_callback (url, data):
-    if url.startswith('http'):
-        response = requests.post(url, json = data)
-        response.raise_for_status()
-
 def extract_origin_pathname (url):
     parsed_url = urlparse(url)
     origin_pathname = parsed_url.scheme + "://" + parsed_url.netloc + parsed_url.path
@@ -71,10 +66,8 @@ def run (job):
         payload['output'] = output
 
         upload_image(upload_url, output_image)
-        webhook_callback(_webhook, json.dumps(payload))
 
         return output
-
     # caught http[s] error
     except requests.exceptions.RequestException as e:
         return { 'error': e.args[0] }
